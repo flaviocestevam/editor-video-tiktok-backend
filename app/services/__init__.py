d9aba6b723bedf88e0a2d86571235280a101163e
flaviocestevam <flaviocostaestevam@gmail.com>
2026-07-17 04:47:17 -0300

    Ativar montagem dinâmica aprimorada

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,1 +1,5 @@
 """Serviços de negócio: download e processamento de vídeo."""
+
+from . import dynamic_montage as _legacy_dynamic_montage
+from . import dynamic_montage_v2 as dynamic_montage
+from . import video_processor
